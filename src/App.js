74d9a9aa63d79b36702c41e65f0a153aad323aa5
AleksRG <aleksandar.kystendil.georgiev@gmail.com>
2021-12-12 22:59:59 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import CollectionCard from "./components/CollectionCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import HeroList from "./components/HeroList";
+import Mein from "./components/Mein";
 
 function App() {
+  const [heroListData, setheroListData] = useState([]);
+  const [selectedHero, setSelectedHero] = useState(0);
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x387E7187Ac91638AF402F5aE08cbb375e97f752E&order_direction=asc"
+      );
+      /*  console.log(openseaData.data.assets); */
+      setheroListData(openseaData.data.assets);
+    };
+    return getMyNfts();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      {heroListData.length > 0 && (
+        <>
+          <Mein heroListData={heroListData} selectedHero={selectedHero} />
+          <HeroList
+            heroListData={heroListData}
+            setSelectedHero={setSelectedHero}
+          />
+        </>
+      )}
     </div>
   );
 }
